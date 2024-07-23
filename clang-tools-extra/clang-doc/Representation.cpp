ac37f51d7aee8fca02059318c127975e48626d2f
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-07-23 18:17:09 -0400

    [clang-doc] switch initialization order for clang-tidy

--- clang-tools-extra/clang-doc/Representation.cpp
+++ clang-tools-extra/clang-doc/Representation.cpp
@@ -371,8 +371,8 @@ ClangDocContext::ClangDocContext(tooling::ExecutionContext *ECtx,
                                  std::vector<std::string> UserStylesheets,
                                  bool FTimeTrace)
     : ECtx(ECtx), ProjectName(ProjectName), PublicOnly(PublicOnly),
-      OutDirectory(OutDirectory), UserStylesheets(UserStylesheets),
-      FTimeTrace(FTimeTrace) {
+      OutDirectory(OutDirectory), FTimeTrace(FTimeTrace),
+      UserStylesheets(UserStylesheets) {
   llvm::SmallString<128> SourceRootDir(SourceRoot);
   if (SourceRoot.empty())
     // If no SourceRoot was provided the current path is used as the default
